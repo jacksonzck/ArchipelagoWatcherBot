7245ee673eeb81443959629a0a3cc14bf243b05d
jazckson <8786660+jacksonzck@users.noreply.github.com>
2026-04-23 14:08:19 -0500

    Updated to better formatting and comment

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,161 @@
 use archipelago_rs::{self as ap, Connection, ConnectionOptions, Event};
-use ustr::{Ustr};
+use ustr::Ustr;
 pub trait MessageLogger {
-    fn log_message(&self) -> ();
+    fn log_message(&self);
 }
 
 impl MessageLogger for &str {
-    fn log_message(&self) -> () {
-        println!("[{}] [{}] {}", std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).expect("Time goes forwards").as_millis(), "Program", &self);
+    // Helper method for helping purely client related information.
+    fn log_message(&self) {
+        println!(
+            "[{}] [Program] {}",
+            std::time::SystemTime::now()
+                .duration_since(std::time::UNIX_EPOCH)
+                .expect("Time goes forwards")
+                .as_millis(),
+            &self
+        );
     }
 }
 
 impl MessageLogger for ap::Error {
-    fn log_message(&self) -> () {
-        println!("[{}] [{}] {:?}", std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).expect("Time goes forwards").as_millis(), "Error", &self);
+    // Logs errors coming from the AP Connection.
+    fn log_message(&self) {
+        println!(
+            "[{}] [Error] {:?}",
+            std::time::SystemTime::now()
+                .duration_since(std::time::UNIX_EPOCH)
+                .expect("Time goes forwards")
+                .as_millis(),
+            &self
+        );
     }
 }
 
 impl MessageLogger for ap::Event {
-    fn log_message(&self) -> () {
-        let timestamp = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).expect("Time goes forwards").as_millis();
+    // Arguably the meat and potatoes of the whole thing.
+    fn log_message(&self) {
+        let timestamp = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .expect("Time goes forwards")
+            .as_millis();
         match &self {
-            Event::Print(print) => println!("[{}] [{}] {}", timestamp, "Archipelago", print),
-            Event::DeathLink { games: _, slots: _, tags: _, time: _, cause, source } => {
+            // Print is everything you would normally see in a normal AP Client - think "JOE sent JOHN a COLD ONE", that sort of thing.
+            // Unfortunetely we can't do more formatting on our end without pulling on the regexes, so I'll leave that as an excercise for the data analyser.
+            Event::Print(print) => println!("[{}] [Archipelago] {}", timestamp, print),
+            // So this is odd - Deathlinks are implemented through the Bounce protocol but archipelago_rs implictly splits them out.
+            // Saves them some work but we'll have to re-do that work in the future if we want to support group deathlinks.
+            Event::DeathLink {
+                games: _,
+                slots: _,
+                tags: _,
+                time: _,
+                cause,
+                source,
+            } => {
                 if let Some(cause) = cause {
-                    println!("[{}] [{}] {} died with message {}", timestamp, "Deathlink", source, cause);
+                    println!(
+                        "[{}] [Deathlink] {} died with message {}",
+                        timestamp, source, cause
+                    );
                 } else {
-                    println!("[{}] [{}] {} died without a message", timestamp, "Deathlink", source)
+                    println!(
+                        "[{}] [Deathlink] {} died without a message",
+                        timestamp, source
+                    )
                 }
-            },
-            Event::KeyChanged { key, old_value, new_value, player } => {
-                let player = player.clone().map_or(String::from("Unknown"), |v| String::from(&*v.name()));
+            }
+            // KeyChanged is used for keeping track of the datastore.
+            // This is kinda a pain in the but because we also have to explictly tell Archipelago we want to be informed about changes to the datastore first;
+            // Which we do in the actual connection code.
+            // Note that all of this will have to be modified in the hypothetical future where teams exist.
+            // But they don't, so we won't.
+            Event::KeyChanged {
+                key,
+                old_value,
+                new_value,
+                player,
+            } => {
+                let player = player
+                    .clone()
+                    .map_or(String::from("Unknown"), |v| String::from(&*v.name()));
                 let key: &str = key;
+                // GiftBoxes are in the format GiftBox;I;J, where I is the team number (always 0) and J is the slotnumber.
+                // We've previously in the connection code told archipelago we care about *every* gift box.
+                // Unfortunately the exact contents are - well, there's a *schema*, but it's nested and all that nonsense.
+                // Potential future improvement to actually format this better but right now we're doing it the lazy way.
                 if key.contains("GiftBox") {
-                    println!("[{}] [Gifting] {} changed giftbox {} from {:?} to content {:?}", timestamp, player, key, old_value, new_value)
+                    println!(
+                        "[{}] [Gifting] {} changed giftbox {} from {:?} to content {:?}",
+                        timestamp, player, key, old_value, new_value
+                    )
+                // The 0 in the EnergyLink key there designates the team we're looking at.
+                // Since teams do not exist it's always 0 haha
+                // This time we're actually nicely formatting it for human reading.
                 } else if key == "EnergyLink0" {
-                    println!("[{}] [Energylink] {} changed energylink from {:?} to value {:?}", timestamp, player, old_value, new_value)
-                } 
-            },
+                    let old_value = match old_value {
+                        Some(serde_json::Value::Number(num)) => num.clone(),
+                        _ => serde_json::Number::from(0),
+                    };
+                    let new_value = new_value
+                        .as_number()
+                        .unwrap_or(&serde_json::Number::from(0))
+                        .clone();
+
+                    println!(
+                        "[{}] [Energylink] {} changed energylink from {} to value {}",
+                        timestamp, player, old_value, new_value
+                    )
+                }
+            }
+            // ReceivedItems is important if we were, like, actually the game we said we were.
+            // But we're not, so it's not.
             Event::ReceivedItems(_) => (),
-            Event::Bounce { games: _, slots: _, tags, data} => {
+            // Bounce Packets are how all of the special protocols besides Energylink and Gifting do their work.
+            // It's basically telling the archipelago server to send arbitrary data to arbitrary games.
+            // Note that as explained above Deathlink technically functions the same way but archipelago_rs had our back then.
+            Event::Bounce {
+                games: _,
+                slots: _,
+                tags,
+                data,
+            } => {
                 if let Some(tagset) = tags {
+                    // Ringlink bounces tell us the source, the amount of rings, and a timestamp (which we're ignoring)
+                    // Unfortunately "Source" in this context means a specific UUID of each connection, which... isn't helpful.
+                    // Oh well.
                     if tagset.contains(&Ustr::from("RingLink")) {
                         let data: Option<serde_json::Value> = data.clone();
                         let data: serde_json::Value = match data {
                             Some(value) => value.clone(),
                             None => return,
                         };
-                        println!("[{}] [{}] UUID {} sent {} rings", timestamp, "RingLink", data["source"], data["amount"]);   
+                        println!(
+                            "[{}] [RingLink] UUID {} sent {} rings",
+                            timestamp, data["source"], data["amount"]
+                        );
+                    // Traplink actually tells us the source and the trap name (and still a timestamp we are subsituting with our own).
+                    // Note that "trap name" in this context is a genericized name.
                     } else if tagset.contains(&Ustr::from("TrapLink")) {
                         let data: Option<serde_json::Value> = data.clone();
                         let data: serde_json::Value = match data {
                             Some(value) => value.clone(),
                             None => return,
                         };
-                        println!("[{}] [{}] {} sent trap {}", timestamp, "TrapLink", data["source"], data["trap_name"]);
+                        println!(
+                            "[{}] [TrapLink] {} sent trap {}",
+                            timestamp, data["source"], data["trap_name"]
+                        );
                     }
                 }
-            },
+            }
+            // Updated tells us if, like, meta-information about the multiworld has changed.
+            // Stuff like if the hint price changed or whatever.
+            // Not really important to us.
             Event::Updated(_updated_fields) => (),
-            Event::Connected => println!("[{}] [{}] {}", timestamp, "Program", "Connected Succesfully!"),
-            Event::Error(_error) => (), // Errors are handled in the main event logic.
+            Event::Connected => println!("[{}] [Program] Connected Succesfully!", timestamp),
+            // The actual error information is in the connection.
+            Event::Error(_error) => (),
         }
     }
 }
@@ -71,7 +167,13 @@ fn main() {
         &*url,
         &*slot_name,
         None::<String>,
-        ConnectionOptions::new().tags([ap::tags::TRACKER, ap::tags::TEXT_ONLY, ap::tags::DEATH_LINK, "RingLink", "TrapLink"]),
+        ConnectionOptions::new().tags([
+            ap::tags::TRACKER,
+            ap::tags::TEXT_ONLY,
+            ap::tags::DEATH_LINK,
+            "RingLink",
+            "TrapLink",
+        ]),
     );
     let mut watching_keys = false;
     loop {
@@ -79,14 +181,22 @@ fn main() {
         if connection.is_connected() {
             if !watching_keys {
                 let mut watched_keys = vec![format!("EnergyLink0")];
-                for player in connection.client().expect("We literally just checked we're connected").players() {
+                // Iterate over every player and watch their gift box.
+                for player in connection
+                    .client()
+                    .expect("We literally just checked we're connected")
+                    .players()
+                {
                     let key = format!("GiftBox;0;{}", player.slot());
                     watched_keys.push(key);
                 }
-                println!("{:?}", watched_keys);
-                match connection.client_mut().expect("We literally just checked we're connected").watch(watched_keys) {
-                    Ok(()) => {watching_keys = true; println!("Watching those keys!")},
-                    Err(error) => error.log_message(), 
+                match connection
+                    .client_mut()
+                    .expect("We literally just checked we're connected")
+                    .watch(watched_keys)
+                {
+                    Ok(()) => watching_keys = true,
+                    Err(error) => error.log_message(),
                 }
             }
             for event in events {
